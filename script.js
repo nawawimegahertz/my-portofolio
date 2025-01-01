0475a7d045d405b4d3a58b49b7711df904fe45a4
Nawawi Megahertz Hybrid++ <59338146+nawawimegahertz@users.noreply.github.com>
2025-01-01 20:33:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Toggle Dark Mode
 const themeToggleBtn = document.getElementById('theme-toggle');
-
 themeToggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
+});
+
+// Responsive Menu Toggle
+const menuToggleBtn = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggleBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
 });
